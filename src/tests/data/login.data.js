b2b4438d22857cbd3876c4ff07516a54c0dd428e
Ramazan Birnazarov <ramazanbirnazarov@gmail.com>
2024-07-09 16:17:10 +0600

    added data provider

--- src/tests/data/login.data.js
+++ src/tests/data/login.data.js
@@ -4,18 +4,21 @@ const loginData = [
     username: "standard_user",
     password: "secret_sauce",
     expectedError: "Username is required",
+    description: "should display an error when username is not entered",
   },
   {
     id: "UC-2",
     username: "standard_user",
-    password: "",
+    password: "secret_sauce",
     expectedError: "Password is required",
+    description: "should display an error when password is not entered",
   },
   {
     id: "UC-3",
     username: "standard_user",
     password: "secret_sauce",
     expectedTitle: "Swag Labs",
+    description: "should login with valid credentials",
   },
 ];
 
--- src/tests/specs/login.tests.js
+++ src/tests/specs/login.tests.js
@@ -1,5 +1,6 @@
 import { LoginPage } from "../../po/pages/login.page.js";
 import { InventoryPage } from "../../po/pages/inventory.page.js";
+import loginData from "../data/login.data.js";
 
 const loginPage = new LoginPage();
 const inventoryPage = new InventoryPage();
@@ -9,48 +10,32 @@ describe("login page", () => {
     await loginPage.open();
   });
 
-  it("UC-1: should display an error when username is not entered", async () => {
-    const { usernameInput, passwordInput, loginButton, errorMessage } =
-      loginPage.loginComponent;
+  loginData.forEach((testCase) => {
+    it(`${testCase.id}: ${testCase.description}`, async () => {
+      const { usernameInput, passwordInput, loginButton, errorMessage } =
+        loginPage.loginComponent;
 
-    await usernameInput.setValue("standard_user");
-    await passwordInput.setValue("secret_sauce");
+      await usernameInput.setValue(testCase.username);
+      await passwordInput.setValue(testCase.password);
 
-    await usernameInput.clearValue();
-    await passwordInput.clearValue();
+      if (testCase.id === "UC-1") {
+        await usernameInput.clearValue();
+        await passwordInput.clearValue();
+      } else if (testCase.id === "UC-2") {
+        await passwordInput.clearValue();
+      }
 
-    await loginButton.click();
+      await loginButton.click();
 
-    await expect(errorMessage).toBeDisplayed();
-    const errorText = await errorMessage.getText();
-    expect(errorText).toContain("Username is required");
-  });
-
-  it("UC-2: should not login with only username credentials", async () => {
-    const { usernameInput, passwordInput, loginButton, errorMessage } =
-      loginPage.loginComponent;
-
-    await usernameInput.setValue("standard_user");
-    await passwordInput.setValue("secret_sauce");
-
-    await passwordInput.clearValue();
-    await loginButton.click();
-
-    await expect(errorMessage).toBeDisplayed();
-    const errorText = await errorMessage.getText();
-    expect(errorText).toContain("Password is required");
-  });
-
-  it("UC-3: should login with valid credentials", async () => {
-    const { usernameInput, passwordInput, loginButton } =
-      loginPage.loginComponent;
-    const { title } = inventoryPage.headerComponent;
-
-    await usernameInput.setValue("standard_user");
-    await passwordInput.setValue("secret_sauce");
-    await loginButton.click();
-
-    const titleText = await title.getText();
-    expect(titleText).toContain("Swag Labs");
+      if (testCase.expectedError) {
+        await expect(errorMessage).toBeDisplayed();
+        const errorText = await errorMessage.getText();
+        expect(errorText).toContain(testCase.expectedError);
+      } else if (testCase.expectedTitle) {
+        const { title } = inventoryPage.headerComponent;
+        const titleText = await title.getText();
+        expect(titleText).toContain(testCase.expectedTitle);
+      }
+    });
   });
 });
